3de87a235e4e2d848de803a4ac698b6bbd57d6ec
rovo98 <2402323937@qq.com>
2018-03-02 14:28:50 +0800

    Update InsertionSortList.java

--- linkedlist/medium/InsertionSortList.java
+++ linkedlist/medium/InsertionSortList.java
@@ -55,7 +55,7 @@ public class InsertionSortList {
         return sorted;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
 
     public static void main(String[] args) {
         InsertionSortList isl = new InsertionSortList();
