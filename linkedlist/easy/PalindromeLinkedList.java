87a27fbad67d8012686bac54d070d3efb9c88cb8
rovo98 <2402323937@qq.com>
2018-03-02 14:24:14 +0800

    Update PalindromeLinkedList.java

--- linkedlist/easy/PalindromeLinkedList.java
+++ linkedlist/easy/PalindromeLinkedList.java
@@ -65,7 +65,7 @@ public class PalindromeLinkedList {
         return true;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         PalindromeLinkedList pl = new PalindromeLinkedList();
         // test list : 1->2->3->2->1->NULL.
