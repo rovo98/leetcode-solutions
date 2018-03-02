6c5e4e793788583bfae6a2abdcb82bcbf19a60ad
rovo98 <2402323937@qq.com>
2018-03-02 14:30:07 +0800

    Update OddEvenLinkedList.java

--- linkedlist/medium/OddEvenLinkedList.java
+++ linkedlist/medium/OddEvenLinkedList.java
@@ -59,7 +59,7 @@ public class OddEvenLinkedList {
         return oddNodes;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         OddEvenLinkedList oel = new OddEvenLinkedList();
         // test list : 1->2->3->4->5->NULL.
