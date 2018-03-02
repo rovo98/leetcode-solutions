db6c1dfe543b92b024ee0219d352d9a96e19ef1b
rovo98 <2402323937@qq.com>
2018-03-02 14:25:14 +0800

    Update RemoveLinkedListElements.java

--- linkedlist/easy/RemoveLinkedListElements.java
+++ linkedlist/easy/RemoveLinkedListElements.java
@@ -36,7 +36,7 @@ public class RemoveLinkedListElements {
         return head;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         RemoveLinkedListElements rle = new RemoveLinkedListElements();
         ListNode testList = new ListNode(-1);
