42ab5f963db2d21d5628b231c93a7701df2dfa78
rovo98 <2402323937@qq.com>
2018-03-02 14:30:33 +0800

    Update PartitionList.java

--- linkedlist/medium/PartitionList.java
+++ linkedlist/medium/PartitionList.java
@@ -59,7 +59,7 @@ public class PartitionList {
         return lessList;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         PartitionList pl = new PartitionList();
         // test list : 1->4->3->2->5->2->NULL. x = 3
