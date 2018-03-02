7025fbd312bed9da90f3138836cae431202746b5
rovo98 <2402323937@qq.com>
2018-03-02 14:16:43 +0800

    Update MoveZeroes.java

--- array/easy/MoveZeroes.java
+++ array/easy/MoveZeroes.java
@@ -43,7 +43,7 @@ public class MoveZeroes {
         }
         System.out.println();
     }
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         MoveZeroes mz = new MoveZeroes();
         int[] testArr = {0, 1, 0, 3, 12};
