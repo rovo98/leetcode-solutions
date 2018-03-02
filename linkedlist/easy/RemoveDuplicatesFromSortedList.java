ee470720368b9e101720bdb257248ab5132db6b8
rovo98 <2402323937@qq.com>
2018-03-02 14:24:39 +0800

    Update RemoveDuplicatesFromSortedList.java

--- linkedlist/easy/RemoveDuplicatesFromSortedList.java
+++ linkedlist/easy/RemoveDuplicatesFromSortedList.java
@@ -58,7 +58,7 @@ public class RemoveDuplicatesFromSortedList {
          }
          return head;
      }
-     // Derive the program to test the method above.
+     // Driver the program to test the method above.
      public static void main(String[] args) {
          RemoveDuplicatesFromSortedList rdfsl = new RemoveDuplicatesFromSortedList();
          // test list1 : 1->2->1->3->3->NULL.
