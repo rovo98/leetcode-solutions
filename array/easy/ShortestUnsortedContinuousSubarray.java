78728c4fa5cd11da1e5009f9d2bbe864dd9984a6
rovo98 <2402323937@qq.com>
2018-03-02 14:20:46 +0800

    Update ShortestUnsortedContinuousSubarray.java

--- array/easy/ShortestUnsortedContinuousSubarray.java
+++ array/easy/ShortestUnsortedContinuousSubarray.java
@@ -55,7 +55,7 @@ public class ShortestUnsortedContinuousSubarray {
         return luIndex - fuIndex + 1;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         ShortestUnsortedContinuousSubarray sucsa = new ShortestUnsortedContinuousSubarray();
         int[] testArr = {2, 6, 4, 8, 10, 9, 15};
