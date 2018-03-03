4b508bfd3d68313cf29f3f9f0c90f4c95652e92b
rovo98 <2402323937@qq.com>
2018-03-03 21:11:12 +0800

    Update SearchInsertPosition.java

--- binarySearch/easy/SearchInsertPosition.java
+++ binarySearch/easy/SearchInsertPosition.java
@@ -37,7 +37,7 @@ public class SearchInsertPosition {
         return low;
     }
 
-    // Derive the program to test the method above.
+    // Driver the program to test the method above.
     public static void main(String[] args) {
         SearchInsertPosition sip = new SearchInsertPosition();
         int[] testArr = {1, 3, 5, 6};
