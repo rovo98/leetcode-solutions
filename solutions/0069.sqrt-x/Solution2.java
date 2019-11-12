b5f17399a79ac572d1a53fa0faeac696a34419b2
rovo98 <rovo98@qq.com>
2019-11-12 22:15:36 +0800

    update * update sqrt-x README.md * update Solution2.java fix bug

--- solutions/0069.sqrt-x/Solution2.java
+++ solutions/0069.sqrt-x/Solution2.java
@@ -1,6 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x < 0) throw IllegalArgumentException();
+        if (x < 0) throw new IllegalArgumentException();
         if (x == 0) return 0;
         double last = 0;
         double res = 1;
