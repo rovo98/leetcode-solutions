7c2335b1db1641a3c087a3e47054e2c4db3282e4
rovo98 <rovo98@163.com>
2021-09-15 00:10:44 +0800

    init repo

---
+++ src/test/java/com/rovo98/leetcode/AppTest.java
@@ -0,0 +1,20 @@
+package com.rovo98.leetcode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
